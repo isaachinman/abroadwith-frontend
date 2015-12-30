21b020e7f340d935b925793739542b05cd4d14c9
Matheus Borges Teixeira <matheus.bt@gmail.com>
2015-12-30 14:35:31 +0100

    Removing HTTPS redirection.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
 var express = require('express');
 var basicAuth = require('basic-auth-connect');
 var http = require('http');
-var https = require('https');
 var nunjucks = require('nunjucks');
 var fs = require('fs');
 var searchRouter = require('./search/Router');
 var mainRouter = require('./main/Router');
 
-var options = {
-  key: fs.readFileSync('test-key.pem'),
-  cert: fs.readFileSync('test-cert.pem')
-};
-
 var app = express();
 
 nunjucks.configure('src',{watch:true});
-
-//app.use(basicAuth('abroadwith', 'betahaus'));
 
 app.use(express.static('build'));
 
@@ -40,13 +32,9 @@ app.get(['/homes/*','/:language/homes/*'], function (req, res) {
 
 app.use(['/search','/:language/search'],searchRouter);
 
-// Create an HTTP service.
-// Redirect from http port to https
-http.createServer(function (req, res) {
-    console.log(new Date().toISOString()+": Redirect -- "+"https://" + req.headers['host'].replace("8080","8443") + req.url);
-    res.writeHead(301, { "Location": "https://" + req.headers['host'].replace("8080","8443") + req.url });
-    res.end();
-}).listen(8080);
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(8443);
+  console.log('Listening at http://%s:%s', host, port);
+});
