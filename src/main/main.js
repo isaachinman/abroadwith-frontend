b70c5fef6d7e2d2b80ff83b92a46f3eb74b727b1
isaachinman <isaacshinman@gmail.com>
2015-12-31 12:26:59 -0800

    Material UI datepicker now working

--- src/main/main.js
+++ src/main/main.js
@@ -2,19 +2,24 @@ var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// Tap events
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
 // Modal deps
 var modal = require('react-modal');
 var HowDoesItWork = require('./components/how-does-it-work.react');
 
 // Datepicker deps
-var DatePicker = require('./components/datepicker.react');
+var DatePicker = require('material-ui/lib/date-picker/date-picker');
+var DatePickerDialog = require('material-ui/lib/date-picker/date-picker-dialog');
 
 ReactDOM.render(
   <HowDoesItWork />,
   document.getElementById('how-does-it-work')
 );
 
-// ReactDOM.render(
-//   <DatePicker />,
-//   document.getElementById('datepicker')
-// );
+ReactDOM.render(
+  <DatePicker hintText="Arrival" />,
+  document.getElementById('arrival')
+);
