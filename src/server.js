f1e785a9e66c7cd7f9794275ebf2111bf349bd81
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-01-04 16:45:19 +0100

    Changing responses to work locally.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var basicAuth = require('basic-auth-connect');
 var http = require('http');
+var https = require('https');
 var nunjucks = require('nunjucks');
 var fs = require('fs');
 var searchRouter = require('./search/Router');
 var mainRouter = require('./main/Router');
 
 var app = express();
+
+var options = {
+  key: fs.readFileSync('test-key.pem'),
+  cert: fs.readFileSync('test-cert.pem')
+};
 
 nunjucks.configure('src',{watch:true});
 
@@ -20,7 +26,7 @@ app.param('language', function (req, res, next, value) {
 });
 
 app.get('/', function (req, res) {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + "/home" });
+  res.writeHead(307, { "Location": req.headers['host'].replace("3000","3443") + "/home" });
   res.end();
 });
 
@@ -32,9 +38,13 @@ app.get(['/homes/*','/:language/homes/*'], function (req, res) {
 
 app.use(['/search','/:language/search'],searchRouter);
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// Create an HTTP service.
+// Redirect from http port to https
+https.createServer(options, function (req, res) {
+    console.log(new Date().toISOString()+": Redirect -- "+"http://" + req.headers['host'].replace("3443","3000") + req.url);
+    res.writeHead(301, { "Location": "http://" + req.headers['host'].replace("3443","3000") + req.url });
+    res.end();
+}).listen(3443);
 
-  console.log('Listening at http://%s:%s', host, port);
-});
+// Create an HTTPS service identical to the HTTP service.
+http.createServer(app).listen(3000);
