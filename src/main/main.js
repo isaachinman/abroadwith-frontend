113e0d24f3d6789c38e0041109cce7130c21e4b1
isaachinman <isaacshinman@gmail.com>
2016-01-12 10:32:20 +0100

    Starting build yet again, this time with Materialize

--- src/main/main.js
+++ src/main/main.js
@@ -10,22 +10,8 @@ injectTapEventPlugin();
 var modal = require('react-modal');
 var HowDoesItWork = require('./components/how-does-it-work.react');
 
-// Render modal
-ReactDOM.render(
-  <HowDoesItWork />,
-  document.getElementById('how-does-it-work')
-);
-
-// Dropdown script
-if ($('dropdown-button').length) {
-  $('.dropdown-button').click(function() {
-      var $button, $menu;
-      $button = $(this);
-      $menu = $button.siblings(".dropdown-menu");
-      $menu.toggleClass("show-menu");
-      $menu.children("li").click(function() {
-        $menu.removeClass("show-menu");
-        $button.html($(this).html());
-      });
-  });
-}
+// // Render modal
+// ReactDOM.render(
+//   <HowDoesItWork />,
+//   document.getElementById('how-does-it-work')
+// );
