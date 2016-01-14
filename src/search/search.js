286b699c9a5b4360f44890a3847282b5becc1063
isaachinman <isaacshinman@gmail.com>
2016-01-14 16:09:40 +0100

    Added i18next and React-NoUiSlider to devDeps

--- src/search/search.js
+++ src/search/search.js
@@ -1,8 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var SearchList = require('./components/searchList.react');
+var Nouislider = require('react-nouislider');
 
 ReactDOM.render(
   <SearchList />,
   document.getElementById('content-search')
 );
+
+ReactDOM.render(
+  <Nouislider
+    range={{min: 0, max: 2000}}
+    start={[1200, 1800]}
+    tooltips
+  />, document.querySelector('#price-slider')
+);
+
+// Booking datepicker
+$(document).ready(function() {
+  if ($('.datepicker').length) {
+    $('.datepicker').pickadate({
+      min: 1,
+      onSet: function () {
+        this.close();
+      }
+    });
+  }
+})
