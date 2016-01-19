718b0fc00274d556e42cf3aca9a0013ce0492f22
isaachinman <isaacshinman@gmail.com>
2016-01-19 15:29:51 +0100

    How does this look as a signup object?

--- src/global/signup.js
+++ src/global/signup.js
@@ -1,1 +1,35 @@
-// Email signup
+{
+  "name": "Isaac",
+  "last-name": "Hinman",
+  "email": "isaac@abroadwith.com",
+  "birthday": "05/18/1993",
+  "languages": {
+    "beginner": [
+      "German"
+    ],
+    "elementary": [ null ],
+    "intermediate-low": [ null ],
+    "intermediate-high": [ null ],
+    "advanced": [ null ],
+    "proficient": [ null ],
+    "native": [
+      "English"
+    ]
+  },
+  "city": "Eisenbahnstr. 19, Berlin 10997, Germany",
+  "profile-photo": "img.abroadwith.com/photo",
+  "school": null,
+  "work": null,
+  "phone": "+49015221448052",
+  "bio": "This is a user bio",
+  "emergency-contact": {
+    "name": "Santa Claus",
+    "phone": "+180080080000",
+    "email": "santa@northpole.com",
+    "relationship": "Father"
+  },
+  "interests": [
+    "JSON",
+    "User signups"
+  ]
+}
