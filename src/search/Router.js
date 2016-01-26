aaba98e033a7cf5dfdeff929129c0410ebf07fe7
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-01-26 16:14:12 +0100

    Adding search skeleton serving files.

--- src/search/Router.js
+++ src/search/Router.js
@@ -12,4 +12,41 @@ router.get('/', function (req, res) {
   res.send(nunjucks.render('search/search.html',context));
 });
 
+router.post('/', function (req, res) {
+  var concat_params = [];
+  for(var param in req.query){
+    concat_params.push(param+"="+req.query[param]);
+  }
+
+  var search_results = {};
+  search_results.params={};
+
+  if(concat_params.length > 0){
+    console.log('../../mockups/search/'+concat_params.join("&")+".json");
+    try{
+      search_results = require('../../mockups/search/'+concat_params.join("&")+'.json');
+    }
+    catch(e){
+      search_results = require('../../mockups/search/default.json');
+    }
+    search_results.params = req.query;
+  }
+  else{
+    search_results = require('../../mockups/search/default.json');
+  }
+
+  if(req.query.lat && req.query.lon){
+    search_results.params.location = {};
+    search_results.params.location.latitude = req.query.lat;
+    search_results.params.location.longitude = req.query.lon;
+  }
+
+  if(req.query.lnglvl){
+    search_results.params.languageCourse = {};
+    search_results.params.languageCourse.level = req.query.lnglvl;
+  }
+
+  res.send(JSON.stringify(search_results));
+});
+
 module.exports = router;
