a58514c2217b14973ae3c61d098a3a3200aa0ea3
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-02-11 15:17:53 +0100

    Fixing regression.

--- src/users/Router.js
+++ src/users/Router.js
@@ -5,10 +5,9 @@ var translations = require('../utils/Translations');
 var router = express.Router();
 
 router.get('/*', function (req, res) {
-  if(req.language && translations[req.language])
-    context = { translations: translations[req.language]};
-  else
-    context = { translations: translations['eng']};
+  context = {
+    translations: req.translations
+  };
   var user_path = "../../mockups/users/";
   if(req.requested_user){
     user_path += req.requested_user;
