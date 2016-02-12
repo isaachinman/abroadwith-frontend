2525cf1ffbdcc302c9aff804f66be5996fe96b44
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-02-12 14:39:59 +0100

    Adjusting context for admin.

--- src/admin/AdminRouter.js
+++ src/admin/AdminRouter.js
@@ -4,10 +4,8 @@ var nunjucks = require('nunjucks');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  context = {
-    translations: req.translations
-  };
-  res.send(nunjucks.render('admin/admin.html',context));
+  if(!req.context) res.status(404).send('No admin context.');
+  res.send(nunjucks.render('admin/admin.html',req.context));
 });
 
 module.exports = router;
