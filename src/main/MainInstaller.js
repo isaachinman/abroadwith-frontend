2bf845973035f8b6d93a1578f7a82725519a31e0
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-02-12 15:26:12 +0100

    Fixing URL for index.

--- src/main/MainInstaller.js
+++ src/main/MainInstaller.js
@@ -1,7 +1,7 @@
 var router = require('./MainRouter');
 
 var installer = function(app) {
-  app.use(['/home','/','index.html?'],router);
+  app.use(['/home','/','/index.html?'],router);
 };
 
 module.exports = installer;
