351aa5f33ac1cd99beeb3d9ca0c8845a85656dcd
isaachinman <isaacshinman@gmail.com>
2016-02-13 13:56:08 +0100

    User edit page now constructing new profile object on save

--- src/users-edit/components/user-edit-profile.react.js
+++ src/users-edit/components/user-edit-profile.react.js
@@ -1,9 +1,27 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  componentDidMount: function() {
+  userEditSave: function() {
 
-    console.log('mounted')
+    // Create new basics object
+    var newProfileObj = {};
+
+    // Fill values
+    $('#about-me').val() !== 'undefined'          ? newProfileObj.aboutMe = $('#about-me').val() : null;
+    $('#education').val() !== 'undefined'         ? newProfileObj.education = $('#education').val() : null;
+    $('#grew-up').val() !== 'undefined'           ? newProfileObj.grewUp = $('#grew-up').val() : null;
+    $('#fav-book').val() !== 'undefined'          ? newProfileObj.favBook = $('#fav-book').val() : null;
+    $('#fav-film').val() !== 'undefined'          ? newProfileObj.favFilm = $('#fav-film').val() : null;
+    $('#amazing-feat').val() !== 'undefined'      ? newProfileObj.amazingFeat = $('#amazing-feat').val() : null;
+    $('#can-share').val() !== 'undefined'         ? newProfileObj.canShare = $('#can-share').val() : null;
+    $('#interests').val() !== 'undefined'         ? newProfileObj.interests = $('#interests').val() : null;
+    $('#countries-visited').val() !== 'undefined' ? newProfileObj.countriesVisited = $('#countries-visited').val() : null;
+    $('#countries-lived').val() !== 'undefined'   ? newProfileObj.countriesLived = $('#countries-lived').val() : null;
+
+    console.log(newProfileObj)
+
+  },
+  componentDidMount: function() {
 
     $.get(this.props.source, function(data) {
 
@@ -11,7 +29,7 @@ module.exports = React.createClass({
       var response = JSON.parse(data);
 
       $('#user-photo').attr('src', response.profile.photo);
-      $('#about-me').html(response.profile.aboutMe);
+      $('#about-me').val(response.profile.aboutMe);
       $('#education').val(response.profile.education);
       $('#grew-up').val(response.profile.grewUp);
       $('#fav-book').val(response.profile.favBook);
@@ -30,6 +48,8 @@ module.exports = React.createClass({
 
   },
   render: function() {
+
+    $('#user-edit-save').click(this.userEditSave);
 
     return (
 
