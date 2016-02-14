feb6d8fa0efda83704c1d92874e8c7908631e9c7
isaachinman <isaacshinman@gmail.com>
2016-02-14 13:36:23 +0100

    Photos tab now working

--- src/manage-home/components/photos-tab.react.js
+++ src/manage-home/components/photos-tab.react.js
@@ -1,76 +1,40 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var PhotoModule = require('./photo-module.react')
 
 module.exports = React.createClass({
-  render: function() {
+  componentDidUpdate: function() {
     if (this.props.photos) {
 
-      var photos = this.props.photos.map(function(url) {
-        return <PhotoModule src={url} key={url} />
+      var photos = this.props.photos;
+
+      console.log(this.props.photos)
+      console.log(photos)
+
+      var PhotoContainer = React.createClass({
+        render: function() {
+          var allPhotos = []
+          photos.forEach(function(url) {
+            allPhotos.push(<PhotoModule src={url} key={url} />)
+          })
+          return (
+            <div>{allPhotos}</div>
+          )
+        }
       })
 
+      ReactDOM.render(
+        <PhotoContainer
+        />, document.querySelector('#photos-container')
+      )
+
     }
+  },
+  render: function() {
+
     return (
 
-      <div id="photos" className="col s12 m10 offset-m1 l10 offset-l1">
-
-        <div className='manage-home-block'>
-
-          <div className='row'>
-            <h4>Bring your home to life with photos</h4>
-          </div>
-
-          <div className='manage-home-section'>
-
-            <div className='row section'>
-              <div className='col s2 m1 l1 input-field center-align grey-text text-lighten-1'>
-                <i className="fa fa-camera-retro fa-2x"></i>
-              </div>
-              <div className="col s8 m9 l9">
-                <form action="#">
-                  <div className="file-field input-field">
-                    <div className="btn">
-                      <span><small>Choose picture</small></span>
-                      <input type="file" />
-                    </div>
-                    <div className="file-path-wrapper">
-                      <input className="file-path validate" type="text" />
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className='col s2 input-field'>
-                <a className='btn-floating'>+</a>
-              </div>
-            </div>
-
-
-          </div>
-
-          <div className='row'></div>
-
-          <div className='manage-home-section'>
-
-            <div className='row manage-home-photos'>
-
-              {photos}
-
-            </div>
-
-          </div>
-
-        </div>
-
-        <div className='row'>
-          <div className='col s6 offset-s3'>
-            <a id='photos-save' className='btn btn-primary save-btn'>Save</a>
-          </div>
-          <div className='col s3 right-align'>
-            <a><i className="fa fa-chevron-right grey-text text-lighten-1 next-btn"></i></a>
-          </div>
-        </div>
-
-      </div>
+      <div></div>
 
     );
   }
