93a8180fc71d7dc1b51bea672d168abb123706d4
isaachinman <isaacshinman@gmail.com>
2016-02-23 11:03:29 +0100

    Removing console logging from i18n

--- src/global/components/i18n.js
+++ src/global/components/i18n.js
@@ -2,7 +2,6 @@
 var i18next = require('i18next');
 var i18nextXHR = require('i18next-xhr-backend');
 
-console.log("This should print ONCE.");
 i18next.use(i18nextXHR).init({
   backend:{
     loadPath: '/locales/{{lng}}/{{ns}}.json'
