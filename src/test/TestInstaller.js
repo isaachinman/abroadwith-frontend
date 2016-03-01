64f07f4c5d5833533011ab8cd89a18f13d587378
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-03-01 22:24:35 +0100

    Adding build chain and testing API.

--- src/test/TestInstaller.js
+++ src/test/TestInstaller.js
@@ -1,7 +1,24 @@
-var router = require('./TestRouter');
+var express = require('express');
+var nunjucks = require('nunjucks');
+
+var router1 = express.Router();
+
+router1.get('/', function (req, res) {
+  if(!req.context) res.status(404).send('No text context.');
+  res.send(nunjucks.render('test/test.html',req.context));
+});
+
+var router2 = express.Router();
+
+router2.get('/', function (req, res) {
+  if(!req.context) res.status(404).send('No text context.');
+  res.send(nunjucks.render('test/build.html',req.context));
+});
+
 
 var installer = function(app) {
-  app.use('/test',router);
+  app.use('/test',router1);
+  app.use('/build',router2);
 };
 
 module.exports = installer;
