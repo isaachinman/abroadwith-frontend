df4520f2a982f93671bc38de6b0dc167604c6c4e
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-03-02 10:51:22 +0100

    Fixing error handling.

--- src/global/middlewares/HomesIdHandler.js
+++ src/global/middlewares/HomesIdHandler.js
@@ -10,18 +10,20 @@ module.exports = function (req, res, next, value) {
   https.get("https://admin.abroadwith.com/users/317/homes/"+value+"/public",
     function (response) {
       var body = '';
+      if(response.statusCode == 404){
+        res.status(404).send('Home not found.');
+        return;
+      }
       response.on('data', function(d) {
           body += d;
       });
       response.on('end', function() {
-
-          // Data reception is done, do whatever with it!
           var parsed = JSON.parse(body);
           req.context.home = parsed;
           next();
       });
   }).on('error', function(e) {
-    res.status(404).send('Home not found.');
-    return;
+    console.log(e);
+    res.status(500).send("Can't connect to API.");
   });
 }
