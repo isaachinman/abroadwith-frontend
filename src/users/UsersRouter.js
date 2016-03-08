d2b924f3e8b4136261f9c97c18da54d73595f757
isaachinman <isaacshinman@gmail.com>
2016-03-08 10:04:15 +0100

    Stay tab on user profile set up correctly

--- src/users/UsersRouter.js
+++ src/users/UsersRouter.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
   if(!req.context) res.status(404).send('No user context.');
+  req.context.debug = JSON.stringify(req.context.user);
   res.send(nunjucks.render('users/users.html',req.context));
+
 });
 
 module.exports = router;
