022cdece0951ed6f93b23a8179513705cc502d0e
isaachinman <isaacshinman@gmail.com>
2016-03-11 12:12:45 +0100

    Setting up placeholder to display if user has no reservations

--- src/reservations/components/reservations-list.react.js
+++ src/reservations/components/reservations-list.react.js
@@ -3,6 +3,7 @@ var ReservationPending = require('./reservation--pending.react');
 var ReservationApproved = require('./reservation--approved.react');
 var ReservationDeclined = require('./reservation--declined.react');
 var ReservationCancelled = require('./reservation--cancelled.react');
+var NoReservations = require('./no-reservations.react');
 
 module.exports = React.createClass({
   render: function() {
@@ -12,35 +13,40 @@ module.exports = React.createClass({
       var reservations = this.props.reservations;
       var reservationsDOM = [];
 
-      for (var i=0; i<reservations.length; i++) {
+      if (reservations.length > 0) {
+        for (var i=0; i<reservations.length; i++) {
 
-        // Render trip component depending on status
-        if (reservations[i].status === 'PENDING') {
-          reservationsDOM.push(
-            <ReservationPending
-              reservation={reservations[i]}
-            />
-          )
-        } else if (reservations[i].status === 'APPROVED') {
-          reservationsDOM.push(
-            <ReservationApproved
-              reservation={reservations[i]}
-            />
-          )
-        } else if (reservations[i].status === 'DECLINED') {
-          reservationsDOM.push(
-            <ReservationDeclined
-              reservation={reservations[i]}
-            />
-          )
-        } else if (reservations[i].status === 'CANCELLED') {
-          reservationsDOM.push(
-            <ReservationCancelled
-              reservation={reservations[i]}
-            />
-          )
+          // Render trip component depending on status
+          if (reservations[i].status === 'PENDING') {
+            reservationsDOM.push(
+              <ReservationPending
+                reservation={reservations[i]}
+              />
+            )
+          } else if (reservations[i].status === 'APPROVED' || reservations[i].status === 'PAID_OUT' || reservations[i].status === 'ARCHIVED') {
+            reservationsDOM.push(
+              <ReservationApproved
+                reservation={reservations[i]}
+              />
+            )
+          } else if (reservations[i].status === 'DECLINED_BY_GUEST' || reservations[i].status === 'DECLINED_BY_HOST' || reservations[i].status === 'DECLINED_AUTOMATICALLY') {
+            reservationsDOM.push(
+              <ReservationDeclined
+                reservation={reservations[i]}
+              />
+            )
+          } else if (reservations[i].status === 'CANCELLED_BY_GUEST' || reservations[i].status === 'CANCELLED_BY_HOST') {
+            reservationsDOM.push(
+              <ReservationCancelled
+                reservation={reservations[i]}
+              />
+            )
+          }
         }
-
+      } else {
+        reservationsDOM.push(
+          <NoReservations />
+        )
       }
     }
 
