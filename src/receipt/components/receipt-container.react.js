ca7811d761bc981ae44872011885a16e818fcb80
isaachinman <isaacshinman@gmail.com>
2016-03-14 13:39:23 +0100

    Fill receipt from endpoints

--- src/receipt/components/receipt-container.react.js
+++ src/receipt/components/receipt-container.react.js
@@ -3,6 +3,8 @@ var React = require('react');
 var jwt_decode = require('jwt-decode');
 var domains = require('domains');
 
+var currencies = require('currencies');
+
 // Component export
 module.exports = React.createClass({
   componentDidMount: function() {
@@ -19,6 +21,36 @@ module.exports = React.createClass({
       success: function(response) {
 
         console.log(response)
+        var currency = currencies[response.currency]
+        $('#bookingCode').html(response.bookingCode);
+        $('#arrivalDate').html(response.arrivalDate);
+        $('#departureDate').html(response.departureDate);
+        $('#duration').html(Math.abs(new Date(response.arrivalDate).getTime() - new Date(response.departureDate).getTime()))
+        $('#guestFullName').html(response.guestFullName);
+        $('#hostFullName').html(response.hostFullName);
+        $('#destination').html(response.destination);
+        $('#destinationShort').html(response.destinationShort);
+        $('#bookingCharges').html(currency+response.bookingCharges);
+        $('#serviceAndVatFees').html(currency+response.serviceAndVatFees);
+        $('#total-charge').html(currency+(response.bookingCharges+response.serviceAndVatFees));
+
+
+      }.bind(this),
+      error: function() {
+
+        alert('Something failed');
+
+      }
+    })
+
+    $.ajax({
+      url: domains.API+'/users/'+JWT.rid+'/bookings/'+bookingId,
+      type: "GET",
+      contentType: "application/json",
+      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('JWT'))},
+      success: function(response) {
+
+
 
       }.bind(this),
       error: function() {
