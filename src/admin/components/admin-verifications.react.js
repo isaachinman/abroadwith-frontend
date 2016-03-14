48eb575eca6229aeab5b02ded3292b390da96ccc
isaachinman <isaacshinman@gmail.com>
2016-03-14 10:49:44 +0100

    Phone and email verifications now working

--- src/admin/components/admin-verifications.react.js
+++ src/admin/components/admin-verifications.react.js
@@ -45,10 +45,56 @@ module.exports = React.createClass({
       beforeSend: function(xhr) {
         xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('JWT'))
       },
-      success: function(response) {
+      success: function(phoneSecret) {
+
+        var newState = {
+          phoneSecret: phoneSecret
+        }
+
+        if (this.isMounted()) {
+          this.setState(newState);
+          console.log(this.state)
+        }
 
         $('#preloader').hide();
         Materialize.toast('Verification SMS sent', 4000);
+
+      }.bind(this),
+      error: function() {
+
+        alert('Something failed');
+
+      }
+    })
+
+  },
+  verifyPhone: function() {
+
+    $('#preloader').show();
+
+    var verifyPhoneObj = {
+      secret: this.state.phoneSecret,
+      key: this.props.phoneNumber,
+      shortCode: parseInt($('#sms-verification-code').val())
+    }
+
+    console.log(verifyPhoneObj)
+
+    var JWT = localStorage.getItem('JWT') !== null ? jwt_decode(localStorage.getItem('JWT')) : null;
+
+    $.ajax({
+      url: domains.API + '/users/' + JWT.rid + '/verification/phone',
+      type: "POST",
+      data: JSON.stringify(verifyPhoneObj),
+      contentType: "application/json",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('JWT'))
+      },
+      success: function(response) {
+
+        console.log(response)
+        $('#preloader').hide();
+        Materialize.toast('Phone verified', 4000);
 
       }.bind(this),
       error: function() {
@@ -70,6 +116,11 @@ module.exports = React.createClass({
     $('a#request-verification-sms').click(function() {
       requestVerificationSMS()
     });
+
+    var verifyPhone = this.verifyPhone;
+    $('a#verify-phone').click(function() {
+      verifyPhone();
+    })
 
 
   },
