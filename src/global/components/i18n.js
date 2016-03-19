fe0c667f9f3f8dd93cd3bc386cdd3b0baa8f516d
Matheus Borges Teixeira <matheus.bt@gmail.com>
2016-03-19 10:07:46 +0100

    Reading the language at client correctly.

--- src/global/components/i18n.js
+++ src/global/components/i18n.js
@@ -1,12 +1,18 @@
 
 var i18next = require('i18next');
 var i18nextXHR = require('i18next-xhr-backend');
+var Cookies = require('js-cookie');
+
+if (!Cookies.get('ui-language')) {
+  console.log("UI language undefined. Setting to English");
+  Cookies.set('ui-language', 'en');
+}
 
 i18next.use(i18nextXHR).init({
   backend:{
     loadPath: '/locales/{{lng}}/{{ns}}.json'
   },
-  lng: 'en',
+  lng: Cookies.get('ui-language'),
   fallbackLng: 'en',
   ns:[]
 });
