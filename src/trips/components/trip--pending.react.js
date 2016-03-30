da85b1763829024851e33dfcd37839eada23ce44
isaachinman <isaacshinman@gmail.com>
2016-03-30 09:34:51 +0200

    Missed this hardcoded link

--- src/trips/components/trip--pending.react.js
+++ src/trips/components/trip--pending.react.js
@@ -25,7 +25,7 @@ module.exports = React.createClass({
 
     var roomPhoto = trip.roomPhoto !== null ? domains.IMG + trip.roomPhoto : '';
     var hostPhoto = trip.hostPhoto ? domains.IMG + trip.hostPhoto : '';
-    var homeLink = domains.FRONTEND + '/homes/' + trip.homeId;
+    var homeLink = '/homes/' + trip.homeId;
 
     var guestWillTeach = trip.languageGuestWillTeach !== null ? i18n.t('languages:'+trip.languageGuestWillTeach) : i18n.t('trips:not_applicable');
 
