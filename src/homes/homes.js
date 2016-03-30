e4c7299c6d650787a27bac30b2c2ba8e8339d8dd
isaachinman <isaacshinman@gmail.com>
2016-03-30 12:32:33 +0200

    Use .first(), not first child

--- src/homes/homes.js
+++ src/homes/homes.js
@@ -29,7 +29,7 @@ if ($('.go-to-booking').length) {
         departureDate:              $('#departure').val(),
         roomId:                     parseInt($(this).attr('data-rid')),
         guestCount:                 1,
-        languageHostWillTeach:      $('.chip--speaks:first-child').attr('data-lang'),
+        languageHostWillTeach:      $('.chip--speaks').first().attr('data-lang'),
         languageGuestWillTeach:     null,
         currency:                   'EUR',
         serviceNames:               null,
