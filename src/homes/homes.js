19afaf745ae7d2e611de787784583e8c8501b74b
isaachinman <isaacshinman@gmail.com>
2016-03-30 11:47:15 +0200

    Fix for date picker tooltip on go-to-booking button

--- src/homes/homes.js
+++ src/homes/homes.js
@@ -10,9 +10,17 @@ if ($('#book-a-room').length) {
 // If on a home page with at least one bookable room
 if ($('.go-to-booking').length) {
 
+  // Click function for book buttons
   $('.go-to-booking').click(function() {
     if ($('#arrival').val() !== '' && $('#departure').val() !== '') {
       window.location = '/homes/' + $(this).attr('data-hid') + '/booking?arrival=' + $('#arrival').val() + '&departure=' + $('#departure').val() + '&room_id=' + $(this).attr('data-rid');
+    }
+  })
+
+  // On datepicker change, validate and remove tooltip if necessary
+  $('#arrival, #departure').change(function() {
+    if ($('#arrival').val() !== '' && $('#departure').val() !== '') {
+      $('.go-to-booking').tooltip('remove');
     }
   })
 
