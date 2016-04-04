fa0577c010f0ea101316083def5523d9382af5a3
isaachinman <isaacshinman@gmail.com>
2016-04-04 17:38:40 +0200

    Updated logout procedure

--- src/global/logout.js
+++ src/global/logout.js
@@ -1,9 +1,16 @@
+var POST = require('POST');
+
 module.exports = function() {
 
   // If a JWT is in localStorage, delete it
   localStorage.getItem('JWT') !== null ? localStorage.removeItem('JWT') : null;
 
-  // Refresh page
-  window.location = '/?has_logged_out=1';
+  var url = '/logout';
+
+  var success = function() {
+    location.reload();
+  }
+
+  POST(url, {}, success)
 
 }
