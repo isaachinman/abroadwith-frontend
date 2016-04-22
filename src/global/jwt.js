38f179481e1a2796c466b07ecf51d135d1c4b33d
isaachinman <isaacshinman@gmail.com>
2016-04-22 17:08:40 +0200

    Now checking for expiry of JWT and logging out if it is expired

--- src/global/jwt.js
+++ src/global/jwt.js
@@ -1,3 +1,13 @@
-const jwt_decode = require('jwt-decode');
+const logout = require('logout')
+const jwt_decode = require('jwt-decode')
 
+
+let expiry = localStorage.getItem('JWT') !== null ? new Date((jwt_decode(localStorage.getItem('JWT'))).exp * 1000) : null;
+let today = new Date()
+
+if (today >= expiry) {
+  logout()
+}
+
+console.log(expiry)
 module.exports = localStorage.getItem('JWT') !== null ? jwt_decode(localStorage.getItem('JWT')) : null;
