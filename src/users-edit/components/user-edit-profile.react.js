f959c4785585616698e33c19ec77086f9823ddd9
isaachinman <isaacshinman@gmail.com>
2016-04-28 17:23:01 +0200

    Debugging

--- src/users-edit/components/user-edit-profile.react.js
+++ src/users-edit/components/user-edit-profile.react.js
@@ -93,6 +93,7 @@ module.exports = React.createClass({
 
           for (key in response) {
             var photo = key.location
+            console.log(photo)
           }
           userObj.photo = photo
 
