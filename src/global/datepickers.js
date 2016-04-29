8550567b961e78f2dc89ec9f3fdc57e7e327ff2a
isaachinman <isaacshinman@gmail.com>
2016-04-29 17:47:45 +0200

    NEW-118

--- src/global/datepickers.js
+++ src/global/datepickers.js
@@ -84,7 +84,7 @@ if ($('input.arrival').length && $('input.departure').length) {
     var translatedDates = {
       previousMonth: i18n.t('common:previousMonth'),
       nextMonth: i18n.t('common:nextMonth'),
-      months: [i18n.t('common:months.m0'), i18n.t('common:months.m1'), i18n.t('common:months.m2'), i18n.t('common:months.m3'), i18n.t('common:months.m4'), i18n.t('common:months.m5'), i18n.t('common:months.m6'), i18n.t('common:months.m7'), i18n.t('common:months.m8'), i18n.t('common:months.m9'), i18n.t('common:months.m10') ,i18n.t('common:months.m11')],
+      months: [i18n.t('common:months.m1'), i18n.t('common:months.m2'), i18n.t('common:months.m3'), i18n.t('common:months.m4'), i18n.t('common:months.m5'), i18n.t('common:months.m6'), i18n.t('common:months.m7'), i18n.t('common:months.m8'), i18n.t('common:months.m9'), i18n.t('common:months.m10'), i18n.t('common:months.m11') ,i18n.t('common:months.m12')],
       weekdays: [i18n.t('common:weekdays.sun'), i18n.t('common:weekdays.mon'), i18n.t('common:weekdays.tue'), i18n.t('common:weekdays.wed'), i18n.t('common:weekdays.thu'), i18n.t('common:weekdays.fri'), i18n.t('common:weekdays.sat')],
       weekdaysShort: [i18n.t('common:weekdaysShort.sun'), i18n.t('common:weekdaysShort.mon'), i18n.t('common:weekdaysShort.tue'), i18n.t('common:weekdaysShort.wed'), i18n.t('common:weekdaysShort.thu'), i18n.t('common:weekdaysShort.fri'), i18n.t('common:weekdaysShort.sat')]
     }
