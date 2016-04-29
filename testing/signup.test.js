b30c1b84d4ce1303c79e54f34c0fdb3e180f0681
isaachinman <isaacshinman@gmail.com>
2016-04-29 12:29:48 +0200

    Email sign up test complete

--- testing/signup.test.js
+++ testing/signup.test.js
@@ -4,24 +4,35 @@ const select2 = require('select2-browserify')
 
 casper.test.begin('Signup test', 5, function(test) {
 
+    casper.options.viewportSize = {
+      width: 1600,
+      height: 950,
+      waitTimeout: 10000
+    }
+
     casper.start('http://localhost:3000', function() {
-      test.assertUrlMatch('http://localhost:3000')
-      test.assertExists('#sign-up-modal')
-    })
-
-    casper.then(function() {
-      this.evaluate(function() {
-        $('.select2.language').last().select2('open')
+      casper.evaluate(function() {
+        localStorage.clear()
       })
+      this.reload()
     })
 
     casper.then(function() {
-      test.assertVisible('ul.select2-results')
+      test.assertUrlMatch('http://localhost:3000')
+    })
+
+    casper.waitForSelector('main', function() {
+      casper.capture('testing/screenshots/pre-signup.png');
+      test.assertExists('#sign-up-modal')
+      this.evaluate(function() {
+        $('a[data-target=choose-languages-modal]').click()
+      })
     })
 
     casper.then(function() {
       this.evaluate(function() {
         $('.select2.language').attr('data-lang','ENG')
+        $('.material.language-level').val('NATIVE')
       })
     })
 
@@ -40,6 +51,41 @@ casper.test.begin('Signup test', 5, function(test) {
     casper.then(function() {
       test.assertVisible('#email-signup')
     })
+
+    casper.then(function() {
+      this.evaluate(function() {
+
+        var email = '';
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < 5; i++ ) {
+          email += possible.charAt(Math.floor(Math.random() * possible.length))
+        }
+        email = email + '@example.com'
+
+        $('#first-name').val('test')
+        $('#last-name').val('test')
+        $('#email').val(email)
+        $('#password').val('blue2222')
+        $('#birthday').val('01-04-1998')
+      })
+    })
+
+    casper.then(function() {
+      this.evaluate(function() {
+        $('#email-signup').click()
+      })
+    })
+
+    casper.waitUntilVisible('#confirmation-email-sent')
+
+    casper.then(function() {
+      test.assertVisible('#confirmation-email-sent')
+      casper.capture('testing/screenshots/signup.png');
+    })
+
+    casper.on("page.error", function(msg, trace) {
+      this.echo("Error: " + msg, "ERROR");
+    });
 
     casper.run(function() {
       test.done()
