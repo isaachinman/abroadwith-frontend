d11329c53ec7daa5df7b68cee8b7e44f33bb5500
isaachinman <isaacshinman@gmail.com>
2016-05-08 12:36:37 +0200

    Optimising non-critical CSS delivery

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 // Disable all console logging for production
 // console.log = function() {}
 
+// Non-critical stylesheets get loaded async here
+var loadDeferredStyles = function() {
+  var addStylesNode = document.getElementById("deferred-styles");
+  var replacement = document.createElement("div");
+  replacement.innerHTML = addStylesNode.textContent;
+  document.body.appendChild(replacement)
+  addStylesNode.parentElement.removeChild(addStylesNode);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+  webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(function() {
+  window.setTimeout(loadDeferredStyles, 0);
+});
+else window.addEventListener('load', loadDeferredStyles);
+
 // jQuery
 require('jquery')
 
