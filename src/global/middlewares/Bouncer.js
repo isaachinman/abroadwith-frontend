1c5cd6671e5e92e01df7c4f4c03f3f84cc9f3686
isaachinman <isaacshinman@gmail.com>
2016-05-13 14:45:47 +0200

    Problem with the bouncer now

--- src/global/middlewares/Bouncer.js
+++ src/global/middlewares/Bouncer.js
@@ -3,7 +3,7 @@ var ServerSettings = require('../../ServerSettings');
 module.exports = function (req, res, next) {
 
   // Only use the bouncer in production
-  if (ServerSettings.strict && !req.logged_user) {
+  if (ServerSettings.strict && !(req.logged_user)) {
     res.redirect("/login")
     return
   }
