0ed63a8c6d5b685640768af4a76c1dac25ca1bdb
isaachinman <isaacshinman@gmail.com>
2016-05-13 15:07:40 +0200

    Refactored Bouncer again

--- src/global/middlewares/Bouncer.js
+++ src/global/middlewares/Bouncer.js
@@ -3,11 +3,10 @@ var ServerSettings = require('../../ServerSettings');
 module.exports = function (req, res, next) {
 
   // Only use the bouncer in production
-  if (ServerSettings.strict) {
-    if(!req.logged_user){
-      res.redirect("/login");
-      return;
-    }
+  if (ServerSettings.strict && !req.logged_user) {
+    res.redirect("/login");
+    return;
   }
   next();
+  
 };
