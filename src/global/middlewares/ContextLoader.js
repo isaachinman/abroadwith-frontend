fda23472dc37ddeb090af2e9dc1c567af2355691
isaachinman <isaacshinman@gmail.com>
2016-05-13 14:28:01 +0200

    First refactor of ContextLoader

--- src/global/middlewares/ContextLoader.js
+++ src/global/middlewares/ContextLoader.js
@@ -10,40 +10,41 @@ var default_bank_currencies = require('../constants/DefaultBankCurrencies');
 var ServerSettings = require('../../ServerSettings');
 
 module.exports = function (req, res, next) {
-  var prefix = req.hostname.substring(0,2);
-  var value = ui_languages[prefix];
-  if(value) {
-    req.language = prefix;
-    res.cookie('ui-language',prefix);
-  }
-  else {
-    if(req.headers['accept-language']){
-      if(ServerSettings.strict){
-        prefix = req.headers['accept-language'].substring(0,2);
-        value = ui_languages[prefix];
-        if(value && prefix != 'en' && !req.cookies['ui-language']){
-          res.cookie('ui-language',prefix);
-          res.writeHead(303, {'Location': "https://"+prefix+ServerSettings.redirect_domain+req.originalUrl});
-          res.end()
-          return;
-        }
-        else{
-          req.language = "en";
-          res.cookie('ui-language',"en");
-        }
-      }
-      else{
-        req.language = "en";
-        res.cookie('ui-language',"en");
-      }
-    }
-    else{
-      req.language = "en";
-      res.cookie('ui-language',"en");
-    }
 
+  // Get language subdomain
+  var prefix = req.hostname.substring(0,2)
+  var value = ui_languages[prefix]
+
+  if (value) {
+
+    // If the user is already on a supported language subdomain, give them that language cookie
+    req.language = prefix
+    res.cookie('ui-language',prefix)
+
+  } else if (ServerSettings.strict && req.headers['accept-language']) {
+
+    // Get accept-language value from browser request header
+    prefix = req.headers['accept-language'].substring(0,2)
+    value = ui_languages[prefix]
+
+    // If the accept-language is not english, set it as needed
+    if(value && prefix != 'en' && !req.cookies['ui-language']){
+      res.cookie('ui-language',prefix);
+      res.writeHead(303, {'Location': "https://"+prefix+ServerSettings.redirect_domain+req.originalUrl})
+      res.end()
+      return
+    } else {
+      req.language = "en"
+      res.cookie('ui-language',"en")
+    }
+  } else {
+    req.language = "en"
+    res.cookie('ui-language',"en")
   }
-  if(!req.context) req.context = {};
+
+  if (!req.context) {
+    req.context = {}
+  }
   req.context.translations = translations[req.language];
   req.context.constants = {
     user_languages: user_languages,
@@ -55,15 +56,15 @@ module.exports = function (req, res, next) {
     domains: domains,
     default_bank_currencies: default_bank_currencies
   }
-  req.context.baseUrl = req.baseUrl;
-  req.context.originalUrl = req.originalUrl;
-  req.context.query = req.query;
+  req.context.baseUrl = req.baseUrl
+  req.context.originalUrl = req.originalUrl
+  req.context.query = req.query
 
   if(req.cookies && req.cookies['ui-currency']){
-    req.context.currency = req.cookies['ui-currency'];
+    req.context.currency = req.cookies['ui-currency']
   }
   else{
-    req.context.currency = 'EUR';
+    req.context.currency = 'EUR'
   }
   next();
 };
