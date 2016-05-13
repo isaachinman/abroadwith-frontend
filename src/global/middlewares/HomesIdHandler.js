00fb97252dc986bbaf04f06e060bc8a0f7ee4172
isaachinman <isaacshinman@gmail.com>
2016-05-13 16:49:38 +0200

    Refactored HomesIdHandler

--- src/global/middlewares/HomesIdHandler.js
+++ src/global/middlewares/HomesIdHandler.js
@@ -16,64 +16,63 @@ module.exports = function (req, res, next, value) {
   }
 
   // Perform a GET request for this home
-  https.get(domains.API + "/public/homes/" + value,
-    function (response) {
+  https.get(domains.API + "/public/homes/" + value, function (response) {
 
-      // Set up an empty string to add
-      var body = '';
+    // Set up an empty string to add to
+    var body = '';
 
-      // If response is a 404, report as such
-      if (response.statusCode == 404) {
-        next('Home not found.')
+    // If response is a 404, report as such
+    if (response.statusCode == 404) {
+      next('Home not found.')
+      return
+    }
+
+    // If response isn't a success, stop here
+    if (response.statusCode != 200) {
+      next('Unexpected response.')
+      return
+    }
+
+    response.on('data', function(d) {
+      body += d
+    })
+
+    response.on('end', function() {
+
+      // Parse the response
+      req.context.home = JSON.parse(body)
+
+      // Double check that the home id was indeed the home returned
+      if (parseInt(value) !== req.context.home.id) {
         return
       }
 
-      // If response isn't a success, stop here
-      if (response.statusCode != 200) {
-        next('Unexpected response.')
+      // Check that home is indeed active
+      if(req.context.home.isActive == false){
+        next('Home is not active.')
         return
       }
 
-      response.on('data', function(d) {
-        body += d
-      })
+      // Do some date parsing, and account for zero indexed months
+      var joined = new Date(Date.parse(req.context.home.host.joinedDate))
+      req.context.home.host.joinedMonth = (joined.getMonth() + 1)
+      req.context.home.host.joinedYear = joined.getFullYear()
 
-      response.on('end', function() {
+      // Loop through rooms and calculate prices based on current currency
+      for (var i=0; i<req.context.home.rooms.length; i++) {
+        req.context.home.rooms[i].price = currency(req.context.home.rooms[i].price,req.context.home.pricing.currency,req.context.currency)
+      }
 
-        // Parse the response
-        req.context.home = JSON.parse(body)
+      // If host has a teacher immersion, calcuate that price too
+      if (req.context.home.immersions && req.context.home.immersions.teacher && req.context.home.immersions.teacher.hourly) {
+        req.context.home.immersions.teacher.hourly = currency(req.context.home.immersions.teacher.hourly,req.context.home.pricing.currency,req.context.currency)
+      }
 
-        // Double check that the home id was indeed the home returned
-        if (parseInt(value) !== req.context.home.id) {
-          return
-        }
+      // Stringify the context
+      req.context.debug = JSON.stringify(req.context.home)
+      next()
 
-        // Check that home is indeed active
-        if(req.context.home.isActive == false){
-          next('Home is not active.')
-          return
-        }
-
-        // Do some date parsing, and account for zero indexed months
-        var joined = new Date(Date.parse(req.context.home.host.joinedDate))
-        req.context.home.host.joinedMonth = (joined.getMonth() + 1)
-        req.context.home.host.joinedYear = joined.getFullYear()
-
-        // Loop through rooms and calculate prices based on current currency
-        for (var i=0; i<req.context.home.rooms.length; i++) {
-          req.context.home.rooms[i].price = currency(req.context.home.rooms[i].price,req.context.home.pricing.currency,req.context.currency)
-        }
-
-        // If host has a teacher immersion, calcuate that price too
-        if (req.context.home.immersions && req.context.home.immersions.teacher && req.context.home.immersions.teacher.hourly) {
-          req.context.home.immersions.teacher.hourly = currency(req.context.home.immersions.teacher.hourly,req.context.home.pricing.currency,req.context.currency)
-        }
-
-        // Stringify the context
-        req.context.debug = JSON.stringify(req.context.home)
-        next()
-
-      })
+    })
   }).on('error', function(e) {
     next("Can't connect to API. Error: " + e)
   })
