795e0c04d92984dd5b4fde07f635fb1b85c6fd12
isaachinman <isaacshinman@gmail.com>
2016-05-18 13:14:30 +0200

    Use imgix for images now

--- src/search/components/search-individual-result.react.js
+++ src/search/components/search-individual-result.react.js
@@ -35,7 +35,7 @@ module.exports = React.createClass({
     if (this.props.homePhotos) {
       var homePhotos = [];
       this.props.homePhotos.forEach(function(src) {
-        var src = domains.IMG + src;
+        var src = domains.IMG + src + '?h=300&auto=compress';
         var photo = <div className="Wallop-item"><img alt={this.props.host} src={src}/></div>
         homePhotos.push(photo);
       }.bind(this))
@@ -48,7 +48,7 @@ module.exports = React.createClass({
 
       var homePhotos = [];
       this.props.homePhotos.forEach(function(src) {
-        var src = domains.IMG + src;
+        var src = domains.IMG + src + '?h=300&auto=compress';
         var photo = <div className="Wallop-item"><img alt={host} data-src={src}/></div>
         homePhotos.push(photo);
       })
@@ -56,7 +56,7 @@ module.exports = React.createClass({
       photos.push(
         <div className="Wallop-list">
           <div className="Wallop-item Wallop-item--current">
-            <img src={domains.IMG + this.props.roomPhoto}/>
+            <img src={domains.IMG + this.props.roomPhoto + '?h=300&auto=compress'}/>
           </div>
           {homePhotos}
         </div>
@@ -67,7 +67,7 @@ module.exports = React.createClass({
       var homePhotos = [];
       var counter = 0;
       this.props.homePhotos.forEach(function(src) {
-        var src = domains.IMG + src;
+        var src = domains.IMG + src + '?h=300&auto=compress';
         counter++
         if (counter === 1) {
           var photo = <div className="Wallop-item"><img alt={host} src={src}/></div>
@@ -87,7 +87,7 @@ module.exports = React.createClass({
 
     // Host photo src
     var hostImg = {
-      backgroundImage: this.props.hostPhoto !== undefined ? 'url(' + domains.IMG + this.props.hostPhoto + ')' : 'url(' + domains.IMG + '/users/default.jpg' + ')'
+      backgroundImage: this.props.hostPhoto !== undefined ? 'url(' + domains.IMG + this.props.hostPhoto + '?w=120&auto=compress' + ')' : 'url(' + domains.IMG + '/users/default.jpg' + '?w=120&auto=compress' + ')'
     }
 
     // Compile immersion tags
