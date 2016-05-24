781b00c154171cc4454d46d56628c54a7af7751d
isaachinman <isaacshinman@gmail.com>
2016-05-24 12:15:38 +0200

    Testing this fix

--- src/upload/ImageUploadInstaller.js
+++ src/upload/ImageUploadInstaller.js
@@ -346,6 +346,9 @@ var installer = function(app) {
   app.use('/upload/users/:photoUserId/homes/:photoHomeId/photos',upload.array('file', 10));
   app.use('/upload/users/:photoUserId/homes/:photoHomeId/photos',routerHome);
 
+  app.use('/de/upload/users/:photoUserId/homes/:photoHomeId/photos',upload.array('file', 10));
+  app.use('/de/upload/users/:photoUserId/homes/:photoHomeId/photos',routerHome);
+
   app.use('/upload/users/:photoUserId/homes/:photoHomeId/rooms/:photoRoomId/photo',upload.array('file', 10));
   app.use('/upload/users/:photoUserId/homes/:photoHomeId/rooms/:photoRoomId/photo',routerRoom);
 
