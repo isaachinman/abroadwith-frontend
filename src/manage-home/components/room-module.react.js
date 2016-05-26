3aeb0582e62900dafe5dd4201d3f2c9f04b9d9c2
isaachinman <isaacshinman@gmail.com>
2016-05-26 11:30:08 +0200

    Testing

--- src/manage-home/components/room-module.react.js
+++ src/manage-home/components/room-module.react.js
@@ -85,6 +85,17 @@ module.exports = React.createClass({
           })
           saveRooms()
         })
+        this.on('success', function(x, serverResponse) {
+          var parsedResponse = JSON.parse(serverResponse)
+          console.log(parsedResponse)
+          console.log(rooms)
+          $.each(serverResponse, function(index, obj) {
+            if (obj.status == 'OK') {
+              room.img = obj.location;
+            }
+          })
+          saveRooms()
+        })
       }
     })
 
