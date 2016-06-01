67d3d5ae2d7f5f1ea45210ac89a957e595f63318
isaachinman <isaacshinman@gmail.com>
2016-06-01 17:15:00 +0200

    Testing

--- src/server.js
+++ src/server.js
@@ -99,7 +99,7 @@ app.post('/logout',function(req,res){
 
   // Remove the access_token cookie
   res.cookie('access_token',"null", { secure:true, httpOnly: true, expires:new Date(0), domain:ServerSettings.cookieDomain })
-  res.clearCookie('access_token', { domain:req.get('host') })
+  // res.clearCookie('access_token', { domain:req.get('host') })
   res.header("Access-Control-Allow-Credentials","true")
   res.sendStatus(200)
 
