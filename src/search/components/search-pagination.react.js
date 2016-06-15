da35800b3ddcb43d5fddef5b266956af6892953c
isaachinman <isaacshinman@gmail.com>
2016-06-15 16:33:33 +0200

    NEW-310

--- src/search/components/search-pagination.react.js
+++ src/search/components/search-pagination.react.js
@@ -6,8 +6,8 @@ module.exports = React.createClass({
     var activePage = Math.ceil(this.props.pageOffset/this.props.pageSize)
 
     // Give active class to current page
-    $('ul#pagination').find('li.active').removeClass('active')
-    $('ul#pagination .page-btn:nth-child(' + ((this.props.pageOffset/10)+2) + ')').addClass('active')
+    // $('ul#pagination').find('li.active').removeClass('active')
+    // $('ul#pagination .page-btn:nth-child(' + ((this.props.pageOffset/10)+2) + ')').addClass('active')
 
     // Remove previous event listener
     $('li.prev').off()
@@ -27,6 +27,7 @@ module.exports = React.createClass({
   render: function() {
 
     var resultCount = this.props.numberOfResults
+
     var pageSize = this.props.pageSize
     var pageOffset = this.props.pageOffset
     var totalPages = Math.ceil(resultCount/pageSize)
@@ -37,39 +38,41 @@ module.exports = React.createClass({
     if (activePage < 1) activePage = 1
     if (totalPages < 1) totalPages = 1
 
-    var pages = [];
+    var pages = []
 
-    for (var i = 1; i <= totalPages; i++) {
+    if (resultCount !== 0) {
+      for (var i = 1; i <= totalPages; i++) {
 
-      var id = 'page-'+i
-      var classes = 'page-btn'
+        var id = 'page-'+i
+        var classes = 'page-btn'
 
-      // If this is active page, add class
-      if (i === activePage) {
-        classes += ' active'
-      }
-
-      // If page is 5 or under, hide all above
-      if (activePage <= 3 && i > 3 && i !== totalPages || i === 5 && activePage <= 3) {
-        classes += ' hide'
-      } else if (activePage > 3 && i !== totalPages && !(i >= totalPages - 3 && activePage > totalPages - 3) && i > 2) {
-
-        // If page is above 5, split the view
-        if (i > activePage + 2 || i < activePage - 1) {
-          classes += ' hide'
+        // If this is active page, add class
+        if (i === activePage) {
+          classes += ' active'
         }
+
+        // If page is 5 or under, hide all above
+        if (activePage <= 3 && i > 3 && i !== totalPages || i === 5 && activePage <= 3) {
+          classes += ' hide'
+        } else if (activePage > 3 && i !== totalPages && !(i >= totalPages - 3 && activePage > totalPages - 3) && i > 2) {
+
+          // If page is above 5, split the view
+          if (i > activePage + 2 || i < activePage - 1) {
+            classes += ' hide'
+          }
+        }
+
+        var newPage = <li id={id} data-page={i} className={classes} onClick={handleChange.bind(this, undefined, (i-1))}><a>{i}</a></li>
+        var ellipsis = <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+
+        if (activePage <= 3 && i === 4) {
+          newPage = ellipsis
+        } else if (activePage > 3 && (i === activePage + 2 && i !== totalPages || i === 2)) {
+          newPage = ellipsis
+        }
+
+        pages.push(newPage)
       }
-
-      var newPage = <li id={id} data-page={i} className={classes} onClick={handleChange.bind(this, undefined, (i-1))}><a>{i}</a></li>
-      var ellipsis = <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
-
-      if (activePage <= 3 && i === 4) {
-        newPage = ellipsis
-      } else if (activePage > 3 && (i === activePage + 2 && i !== totalPages || i === 2)) {
-        newPage = ellipsis
-      }
-
-      pages.push(newPage);
     }
 
     // Compile styling for prev and next buttons
