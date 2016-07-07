d7baad83b3c92ee56427e22fd5b37e00b7b8c1ed
isaachinman <isaacshinman@gmail.com>
2016-07-07 15:55:52 +0200

    Refactoring

--- src/main/main.js
+++ src/main/main.js
@@ -1,19 +1,19 @@
-const Wallop = require('wallop');
-const newMessageThread = require('new-message-thread');
-const refreshToken = require('refresh-token');
+const Wallop = require('wallop')
+const newMessageThread = require('new-message-thread')
+const refreshToken = require('refresh-token')
 
 const jwt_decode = require('jwt-decode')
 
-const validateBookNowButtons = require('validate-book-now-buttons');
-const validateMessageButtons = require('validate-message-buttons');
-validateBookNowButtons();
-validateMessageButtons();
+const validateBookNowButtons = require('validate-book-now-buttons')
+const validateMessageButtons = require('validate-message-buttons')
+validateBookNowButtons()
+validateMessageButtons()
 
 const uiDate = require('ui-date')
 const pikaday = require('pikaday')
 const select2 = require('select2-browserify')
 
-const i18n = require('i18n');
+const i18n = require('i18n')
 
 // Initialisations
 $(document).ready(function() {
@@ -42,33 +42,33 @@ $(document).ready(function() {
 
   if ($('#hero-search').length) {
 
-    $(window).width() < 601 ? $('#location').attr('placeholder', i18n.t('common:where_mobile')) : null;
+    $(window).width() < 601 ? $('#location').attr('placeholder', i18n.t('common:where_mobile')) : null
 
     $('#hero-search').click(function() {
 
-      var language = $('#language option:selected').val() !== '' ? '&language=' + $('#language option:selected').attr('data-lang') : '';
-      var location = $('#location').val() !== '' ? '&location_string=' + ($('#location').val()).replace(/ /g,"_") : '';
-      var arrival = $('#arrival').val() !== '' ? '&arrival=' + $('#arrival').val() : '';
-      var departure = $('#departure').val() !== '' ? '&departure=' + $('#departure').val() : '';
-      var guests = $('#guest-count').val() !== 'undefined' ? '?guests=' + $('#guest-count').val() : '';
+      var language = $('#language option:selected').val() !== '' ? '&language=' + $('#language option:selected').attr('data-lang') : ''
+      var location = $('#location').val() !== '' ? '&location_string=' + ($('#location').val()).replace(/ /g,"_") : ''
+      var arrival = $('#arrival').val() !== '' ? '&arrival=' + $('#arrival').val() : ''
+      var departure = $('#departure').val() !== '' ? '&departure=' + $('#departure').val() : ''
+      var guests = $('#guest-count').val() !== 'undefined' ? '?guests=' + $('#guest-count').val() : ''
 
-      var url = '/search'+ guests + language + location + arrival + departure;
+      var url = '/search'+ guests + language + location + arrival + departure
 
-      window.location = url;
+      window.location = url
 
     })
   }
 
   if ($('form.inert').length) {
     $('form.inert').submit(function(e) {
-      return false;
+      return false
     })
   }
 
   if ($('.wallop').length) {
     $('.wallop').each(function() {
       var wallopEl = this;
-      var slider = new Wallop(wallopEl);
+      var slider = new Wallop(wallopEl)
     })
   }
 
@@ -76,17 +76,17 @@ $(document).ready(function() {
   if ($('.carousel').length) {
     $('.carousel').carousel({
       dist: 0,
-    });
+    })
   }
 
   // Select2 language to learn
   if ($('select#language').length) {
     i18n.loadNamespaces(['search'], function() {
-      var languagePlaceholder = $(window).width() < 601 ? i18n.t('search:language_to_learn_mobile') : i18n.t('search:language_to_learn');
+      var languagePlaceholder = $(window).width() < 601 ? i18n.t('search:language_to_learn_mobile') : i18n.t('search:language_to_learn')
       $("select#language").select2({
         placeholder: languagePlaceholder
-      });
-    });
+      })
+    })
   }
 
   // Select2 language to teach
@@ -94,18 +94,18 @@ $(document).ready(function() {
     i18n.loadNamespaces(['search'], function() {
       $("select#language-teach").select2({
         placeholder: i18n.t('search:language_to_teach')
-      });
-    });
+      })
+    })
   }
 
   // Modal
   if ($('.modal-trigger').length) {
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal()
   }
 
   // Select
   if ($('select.material').length) {
-    $('select.material').material_select();
+    $('select.material').material_select()
     $("select.material[required]").css({
       display: "block",
       height: '1px',
@@ -114,12 +114,12 @@ $(document).ready(function() {
       opacity: 0,
       border: 0,
       position: 'absolute'
-    });
+    })
   }
 
   // Sidenav
   if ($('.button-collapse').length) {
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav()
   }
 
   if ($('.become-a-host--logged-out').length) {
@@ -134,4 +134,4 @@ $(document).ready(function() {
     })
   }
 
-});
+})
