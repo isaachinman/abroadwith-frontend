f49ad97166b0939d13b6544d028b40c48828d66a
isaachinman <isaacshinman@gmail.com>
2016-07-22 10:20:10 +0200

    NEW-403

--- src/verification/VerificationRouter.js
+++ src/verification/VerificationRouter.js
@@ -3,7 +3,6 @@ const nunjucks = require('nunjucks')
 const http = require('http')
 const https = require('https')
 const domains = require('../global/constants/domains')
-const ServerSettings = require('../ServerSettings')
 const jwt = require('jsonwebtoken')
 
 var router = express.Router()
@@ -21,35 +20,24 @@ router.get('/', function(req, res) {
 
   // Set up the request
   var post_req
-  if (domains.API_HTTP == "https" && req.cookies['access_token']) {
+  if (domains.API_HTTP == "https") {
 
-    var decodedToken = jwt.verify(req.cookies['access_token'], new Buffer(ServerSettings.public_key,'utf8'), { algorithms: ['RS512'] }, function (err, payload) {
+    var decodedToken = req.cookies && req.cookies['access_token'] ? jwt.decode(req.cookies['access_token']) : false
+    console.log(decodedToken)
 
-      console.log(payload)
+    post_req = https.request(post_options, function(response) {
 
-      post_req = https.request(post_options, function(response) {
-
-        if (response.statusCode == 204) {
-          res.send(nunjucks.render('verification/email-verified-success.html', req.context))
-        } else if (payload.cbk !== 1 && payload.cbk !== 3) {
-          res.send(nunjucks.render('verification/email-already-verified.html', req.context))
-        } else {
-          res.send(nunjucks.render('verification/email-verified-failure.html', req.context))
-        }
-
-      })
-
-      post_req.on('error', function(e) {
+      if (response.statusCode == 204) {
+        res.send(nunjucks.render('verification/email-verified-success.html', req.context))
+      } else if (decodedToken && decodedToken.cbk !== 1 && decodedToken.cbk !== 3) {
+        res.send(nunjucks.render('verification/email-already-verified.html', req.context))
+      } else {
         res.send(nunjucks.render('verification/email-verified-failure.html', req.context))
-      })
-
-      post_req.end()
+      }
 
     })
 
-  }
-
-  else {
+  } else {
 
     post_req = http.request(post_options, function(response) {
 
@@ -62,6 +50,11 @@ router.get('/', function(req, res) {
 
   }
 
+  post_req.on('error', function(e) {
+    res.send(nunjucks.render('verification/email-verified-failure.html', req.context))
+  })
+
+  post_req.end()
 })
 
 module.exports = router
