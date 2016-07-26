4ad8021b6ad22d6b979fc048b69cb87a5236b192
isaachinman <isaacshinman@gmail.com>
2016-07-26 10:01:44 +0200

    New method of styling

---
+++ src/containers/Home/Home.styles.js
@@ -0,0 +1,18 @@
+import { havelockBlue, accentBg } from 'styles/colors'
+
+export default {
+  hero: {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+    background: havelockBlue,
+    color: `white`,
+    minHeight: 400,
+  },
+  h1: {
+    fontSize: '6rem',
+  },
+  h2: {
+    color: accentBg,
+  },
+}
