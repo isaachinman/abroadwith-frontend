3de11374c55f4469de1cc638c98fee706393438b
isaachinman <isaacshinman@gmail.com>
2016-11-23 15:08:50 +0100

    Set cookie domain

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use('/api', (req, res) => {
 app.post('/logout', (req, res) => {
 
   // Remove the access_token cookie
-  res.cookie('access_token', 'null', { secure: true, httpOnly: true, expires: new Date(0) })
+  res.cookie('access_token', 'null', { secure: true, httpOnly: true, expires: new Date(0), domain: '.test-abroadwith.com' })
   res.header('Access-Control-Allow-Credentials', 'true')
   res.sendStatus(200)
 
