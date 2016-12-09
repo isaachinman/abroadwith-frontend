be3338b9f4beab2a3ef4fb75417506132c784108
isaachinman <isaacshinman@gmail.com>
2016-12-09 13:41:31 +0100

    All three signup types now working

---
+++ src/redux/modules/signup.js
@@ -0,0 +1,80 @@
+import config from 'config.js'
+import superagent from 'superagent'
+import { login, facebookLogin, googleLogin } from 'redux/modules/auth'
+
+// Login stuff
+const SIGNUP = 'auth/SIGNUP'
+const SIGNUP_SUCCESS = 'auth/SIGNUP_SUCCESS'
+const SIGNUP_FAIL = 'auth/SIGNUP_FAIL'
+
+const initialState = {
+  loaded: false,
+}
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case SIGNUP:
+      return {
+        ...state,
+        loading: true,
+      }
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+      }
+    case SIGNUP_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error: true,
+        errorMessage: action.error,
+      }
+    default:
+      return state
+  }
+}
+
+export function signup(type, signupObject, googleToken) {
+  return async dispatch => {
+    try {
+
+      console.log('inside redux function', signupObject, googleToken)
+
+      const { email, password, facebookToken, googleId } = signupObject // eslint-disable-line
+
+      const request = superagent.post(`${config.apiHost}/users`).send(signupObject).withCredentials()
+
+      request.end(err => {
+
+        if (err) {
+
+          dispatch({ type: SIGNUP_FAIL, err })
+
+        } else {
+
+          // Signup was success
+          dispatch({ type: SIGNUP_SUCCESS })
+
+          // Log the user in
+          if (type === 'email') {
+            dispatch(login(email, password))
+          } else if (type === 'facebook') {
+            dispatch(facebookLogin(email, facebookToken))
+          } else if (type === 'google') {
+            dispatch(googleLogin(email, googleToken))
+          }
+
+
+        }
+
+
+      })
+
+    } catch (err) {
+      dispatch({ type: SIGNUP_FAIL, err })
+    }
+  }
+}
