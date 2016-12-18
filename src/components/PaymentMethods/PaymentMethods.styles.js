d7a6310b3e75de4f174ed0e9c40f672f00d06d40
isaachinman <isaacshinman@gmail.com>
2016-12-18 13:50:30 -0800

    Payment methods built out

---
+++ src/components/PaymentMethods/PaymentMethods.styles.js
@@ -0,0 +1,18 @@
+export default {
+  ccFullWidth: {
+    marginBottom: 5,
+  },
+  ccHalfWidth: {
+    display: 'inline-block',
+    maxWidth: 'calc(50% - 2.5px)',
+  },
+  paypalContainer: {
+    padding: '0 10px',
+  },
+  panel: {
+    minHeight: 120,
+  },
+  typeIcon: {
+    float: 'right',
+  },
+}
--- src/components/PaymentMethods/subcomponents/CreditCard.js
+++ src/components/PaymentMethods/subcomponents/CreditCard.js
@@ -1,8 +1,12 @@
 // Absolute imports
 import React, { Component, PropTypes } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Panel } from 'react-bootstrap'
 import { translate } from 'react-i18next'
 import { connect } from 'react-redux'
+import FontAwesome from 'react-fontawesome'
+
+// Relative imports
+import styles from '../PaymentMethods.styles'
 
 @connect(state => ({
   jwt: state.auth.token,
@@ -13,20 +17,27 @@ export default class CreditCard extends Component {
 
   render() {
 
+    const { t, id, lastFour, expiry, deletePaymentMethod } = this.props
+
     return (
-      <Row>
-        <Col xs={12} md={6} lg={3}>
-          Credit card
-        </Col>
-      </Row>
+      <Col xs={12} md={6} lg={3}>
+        <Panel style={styles.panel}>
+          <div>XXXXXXXXXXXX{lastFour}</div>
+          <div>{expiry}</div>
+          <div style={styles.bottomRow}>
+            <a style={styles.removeBtn} onClick={() => deletePaymentMethod(id)}>{t('common.Remove')}</a>
+            <span style={styles.typeIcon}><FontAwesome name='credit-card-alt' /></span>
+          </div>
+        </Panel>
+      </Col>
     )
   }
 }
 
 CreditCard.propTypes = {
-  type: PropTypes.string,
-  user: PropTypes.object,
-  dispatch: PropTypes.func,
-  jwt: PropTypes.object,
+  id: PropTypes.number,
+  expiry: PropTypes.string,
+  lastFour: PropTypes.string,
+  deletePaymentMethod: PropTypes.func,
   t: PropTypes.func,
 }
