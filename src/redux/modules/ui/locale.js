b147ad21b16dff74c36f3ecf18578d2a7c249836
isaachinman <isaacshinman@gmail.com>
2017-01-09 10:59:10 +0100

    Adjustments to locale handling

--- src/redux/modules/ui/locale.js
+++ src/redux/modules/ui/locale.js
@@ -2,12 +2,7 @@ import Cookies from 'js-cookie'
 import superagent from 'superagent'
 import i18n from '../../../i18n/i18n-client.js'
 
-// Load locale
-const LOAD_LOCALE = 'abroadwith/LOAD_LOCALE'
-const LOAD_LOCALE_SUCCESS = 'abroadwith/LOAD_LOCALE_SUCCESS'
-const LOAD_LOCALE_FAIL = 'abroadwith/LOAD_LOCALE_FAIL'
-
-// Change locale (must be loaded first)
+// Change locale
 const CHANGE_LOCALE = 'abroadwith/CHANGE_LOCALE'
 const CHANGE_LOCALE_SUCCESS = 'abroadwith/CHANGE_LOCALE_SUCCESS'
 const CHANGE_LOCALE_FAIL = 'abroadwith/CHANGE_LOCALE_FAIL'
@@ -22,6 +17,7 @@ export default function reducer(state = initialState, action = {}) {
       return {
         ...state,
         loading: true,
+        loaded: false,
       }
     case CHANGE_LOCALE_SUCCESS:
       return {
@@ -37,56 +33,8 @@ export default function reducer(state = initialState, action = {}) {
         loaded: false,
         error: action.error,
       }
-    case LOAD_LOCALE:
-      return {
-        ...state,
-        loading: true,
-      }
-    case LOAD_LOCALE_SUCCESS:
-      return {
-        ...state,
-        loading: true,
-        loaded: true,
-      }
-    case LOAD_LOCALE_FAIL:
-      return {
-        ...state,
-        loading: false,
-        loaded: false,
-        error: action.error,
-      }
     default:
       return state
-  }
-}
-
-export function loadLocale(locale, callback) {
-
-  const cb = typeof callback === 'function' ? callback : () => {}
-
-  return dispatch => {
-    try {
-
-      const request = superagent.get(`/locales/${locale}.json`)
-      request.end((err, res) => {
-
-        window.__i18n = {
-          locale,
-          translations: JSON.parse(res.text),
-        }
-
-        i18n.addResourceBundle(window.__i18n.locale, 'translation', window.__i18n.translations, true)
-        i18n.changeLanguage(window.__i18n.locale)
-
-        cb(locale)
-
-      })
-
-    } catch (err) {
-
-      dispatch({ type: LOAD_LOCALE_FAIL })
-
-    }
   }
 }
 
@@ -102,10 +50,22 @@ export function changeLocale(locale, setCookie) {
 
         // A boolean to control the setting of the cookie will be passed on client-side calls
         Cookies.set('ui_language', locale)
-        dispatch(loadLocale(locale, () => {
+
+        // Load the necessary locale
+        const request = superagent.get(`/locales/${locale}.json`)
+        request.end((err, res) => {
+
+          window.__i18n = {
+            locale,
+            translations: JSON.parse(res.text),
+          }
+
+          i18n.addResourceBundle(window.__i18n.locale, 'translation', window.__i18n.translations, true)
+          i18n.changeLanguage(window.__i18n.locale)
+
           dispatch({ type: CHANGE_LOCALE_SUCCESS, locale })
-          dispatch({ type: LOAD_LOCALE_SUCCESS })
-        }))
+
+        })
 
       } else {
 
