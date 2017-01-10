45043a770fcf920d3623c8068181b4dbcb8d0f73
isaachinman <isaacshinman@gmail.com>
2017-01-10 13:56:39 +0100

    Manage home skeleton built out

--- src/containers/ManageHome/ManageHome.js
+++ src/containers/ManageHome/ManageHome.js
@@ -3,22 +3,32 @@ import React, { Component, PropTypes } from 'react'
 import Helmet from 'react-helmet'
 import { translate } from 'react-i18next'
 import { Grid, Row } from 'react-bootstrap'
+import { asyncConnect } from 'redux-connect'
 import { connect } from 'react-redux'
+import { load as loadHomestayWithAuth } from 'redux/modules/privateData/homes/loadHomeWithAuth'
 
+@asyncConnect([{
+  promise: ({ params, store: { dispatch, getState } }) => {
+
+    return dispatch(loadHomestayWithAuth(getState().auth.token, params.homeID))
+
+  },
+}])
 @connect(
-  state => ({
-    homes: state.privateData.homes,
+  (state, ownProps) => ({
+    home: state.privateData.homes[ownProps.params.homeID],
   }),
 )
 @translate()
 export default class ManageHome extends Component {
   render() {
-    const { homes, t } = this.props
+    const { t } = this.props
+    console.log(this)
     return (
       <Grid>
         <Row>
           <Helmet title={t('manage_home.title')} />
-          {Object.keys(homes).map(homeID => <div>{homeID}</div>)}
+          Single home management page
         </Row>
       </Grid>
     )
--- src/containers/ManageHome/MultiHomeLandingPage.js
+++ src/containers/ManageHome/MultiHomeLandingPage.js
@@ -2,9 +2,10 @@
 import React, { Component, PropTypes } from 'react'
 import Helmet from 'react-helmet'
 import { translate } from 'react-i18next'
-import { Grid, Row } from 'react-bootstrap'
+import { Grid, Row, Panel, Col } from 'react-bootstrap'
 import { asyncConnect } from 'redux-connect'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 import { load as loadHomestayWithAuth } from 'redux/modules/privateData/homes/loadHomeWithAuth'
 
 @asyncConnect([{
@@ -13,8 +14,6 @@ import { load as loadHomestayWithAuth } from 'redux/modules/privateData/homes/lo
 
     const { auth, privateData } = getState()
     const promises = []
-
-    console.log(privateData)
 
     if (privateData.user.data) {
       privateData.user.data.homeIds.map(homeID => {
@@ -33,14 +32,29 @@ import { load as loadHomestayWithAuth } from 'redux/modules/privateData/homes/lo
 )
 @translate()
 export default class MultiHomeLandingPage extends Component {
+
+  redirectToManageHome = homeID => {
+    this.props.dispatch(push(`/manage-home/${homeID}`))
+  }
+
   render() {
     const { homes, t } = this.props
     return (
       <Grid>
+        <Helmet title={t('manage_home.multi_home_title')} />
         <Row>
-          <Helmet title={t('manage_home.title')} />
-          Multi Home Landing Page
-          {Object.keys(homes).map(homeID => <div>{homeID}</div>)}
+          <h1>{t('manage_home.multi_home_title')}</h1>
+        </Row>
+        <Row>
+          {Object.keys(homes).map(homeID => {
+            return (
+              <Col xs={12} md={6} lg={3} key={homeID}>
+                <Panel onClick={() => this.redirectToManageHome(homeID)}>
+                  Home: {homeID}
+                </Panel>
+              </Col>
+            )
+          })}
         </Row>
       </Grid>
     )
@@ -48,6 +62,7 @@ export default class MultiHomeLandingPage extends Component {
 }
 
 MultiHomeLandingPage.propTypes = {
+  dispatch: PropTypes.func,
   homes: PropTypes.object,
   t: PropTypes.func,
 }
