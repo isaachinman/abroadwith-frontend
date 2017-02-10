75e3152ec3ee765c78f2e86d3105e700c4d23ef9
isaachinman <isaacshinman@gmail.com>
2017-02-10 13:53:38 +0100

    Animation jump bug seems to be related to FadeProps module

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -113,7 +113,7 @@ export default class App extends Component {
           <LoadingBar />
           <Navbar jwt={jwt} user={user} title={config.app.title} />
 
-          <FadeProps animationLength={50}>
+          <FadeProps animationLength={200}>
             <main style={styles.appContent} key={routing.pathname}>
               {route.status === 200 && this.props.children}
               {route.status === 404 && <NotFound />}
