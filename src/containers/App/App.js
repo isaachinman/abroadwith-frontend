4161075dc16bc9fbcb95aacb8564328cc8333518
isaachinman <isaacshinman@gmail.com>
2017-02-11 12:46:01 +0100

    Currency conversion built out

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -10,6 +10,7 @@ import React, { Component, PropTypes } from 'react'
 import NotFound from 'components/NotFound/NotFound'
 import { load as loadHomestayWithAuth } from 'redux/modules/privateData/homes/loadHomeWithAuth'
 import { load as loadUserWithAuth } from 'redux/modules/privateData/users/loadUserWithAuth'
+import { loadCurrencyRates } from 'redux/modules/ui/currency'
 import VerifyEmailModal from 'components/Modals/VerifyEmailModal'
 import VerifyPhoneModal from 'components/Modals/VerifyPhoneModal'
 import config from 'config'
@@ -32,6 +33,7 @@ import styles from './App.styles'
 }])
 @connect(
   state => ({
+    currency: state.ui.currency,
     footer: state.ui.footer,
     jwt: state.auth.jwt,
     token: state.auth.token,
@@ -49,13 +51,13 @@ export default class App extends Component {
   }
 
   // -------------------------------------------------------------------/
-  //   Note that componentDidMount doesn't fire on the server
+  //   Note that componentDidMount doesn't call on the server
   //   So these requests will specifically wait to fire on the client,
   //   reducing http request load and other computation on the server
   // ------------------------------------------------------------------/
   componentDidMount = () => {
 
-    const { dispatch, homes, token, user } = this.props
+    const { currency, dispatch, homes, token, user } = this.props
 
     // Load homes if necessary
     if (user.data && user.data.homeIds && homes.length !== user.data.homeIds.length) {
@@ -65,6 +67,12 @@ export default class App extends Component {
         }
       })
     }
+
+    // Load currency rates if for some reason it didn't happen in SSR
+    if (!currency.exchangeRates.loaded || !currency.exchangeRates.data) {
+      dispatch(loadCurrencyRates())
+    }
+
   }
 
   componentWillReceiveProps(nextProps) {
@@ -102,8 +110,6 @@ export default class App extends Component {
 
     const { footer, jwt, user, route, routing } = this.props
 
-    console.log('KEY: ', routing.pathname)
-
     return (
       <StyleRoot>
         <div style={styles.appContainer}>
@@ -140,6 +146,7 @@ export default class App extends Component {
 
 App.propTypes = {
   children: PropTypes.object,
+  currency: PropTypes.object,
   dispatch: PropTypes.func,
   footer: PropTypes.object,
   homes: PropTypes.object,
--- src/redux/modules/ui/currency.js
+++ src/redux/modules/ui/currency.js
@@ -1,4 +1,10 @@
 import Cookies from 'js-cookie'
+import superagent from 'superagent'
+
+// Load currency rates
+const LOAD_CURRENCY_RATES = 'abroadwith/LOAD_CURRENCY_RATES'
+const LOAD_CURRENCY_RATES_SUCCESS = 'abroadwith/LOAD_CURRENCY_RATES_SUCCESS'
+const LOAD_CURRENCY_RATES_FAIL = 'abroadwith/LOAD_CURRENCY_RATES_FAIL'
 
 // Change currency
 const CHANGE_CURRENCY = 'abroadwith/CHANGE_CURRENCY'
@@ -7,10 +13,40 @@ const CHANGE_CURRENCY_FAIL = 'abroadwith/CHANGE_CURRENCY_FAIL'
 
 const initialState = {
   loaded: false,
+  exchangeRates: {
+    loading: false,
+    loaded: false,
+  },
 }
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case LOAD_CURRENCY_RATES:
+      return {
+        ...state,
+        exchangeRates: {
+          loading: true,
+          loaded: false,
+        },
+      }
+    case LOAD_CURRENCY_RATES_SUCCESS:
+      return {
+        ...state,
+        exchangeRates: {
+          data: action.result,
+          loading: false,
+          loaded: true,
+        },
+      }
+    case LOAD_CURRENCY_RATES_FAIL:
+      return {
+        ...state,
+        exchangeRates: {
+          error: action.error,
+          loading: false,
+          loaded: false,
+        },
+      }
     case CHANGE_CURRENCY:
       return {
         ...state,
@@ -59,6 +95,44 @@ export function changeCurrency(currency, setCookie, callback) {
 
       dispatch({ type: CHANGE_CURRENCY_FAIL, err })
 
+    }
+  }
+}
+
+// This function is primarily called by the server on initial session request,
+// but can be called on the client if the SSR call fails
+export function loadCurrencyRates(callback) {
+
+  const cb = typeof callback === 'function' ? callback : () => {}
+
+  return async dispatch => {
+
+    dispatch({ type: LOAD_CURRENCY_RATES })
+
+    try {
+
+      // Daily currency rates are fetched and stored on an s3 bucket by an external application
+      const request = superagent.get('https://d9fbkd6o04txh.cloudfront.net/latest.json')
+      request.end((error, response = {}) => {
+
+        if (error) {
+
+          dispatch({ type: LOAD_CURRENCY_RATES_FAIL, error })
+          cb()
+
+        } else {
+
+          // GET was successful
+          dispatch({ type: LOAD_CURRENCY_RATES_SUCCESS, result: JSON.parse(response.text) })
+          cb()
+
+        }
+
+      })
+
+    } catch (error) {
+      dispatch({ type: LOAD_CURRENCY_RATES_FAIL, error })
+      cb()
     }
   }
 }
