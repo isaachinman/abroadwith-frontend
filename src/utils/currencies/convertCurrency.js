312b58f07bc4004dffcc1bcc5a8d46d3c3fb1142
isaachinman <isaacshinman@gmail.com>
2017-02-13 17:16:31 +0100

    Float all prices to 2 decimals

--- src/utils/currencies/convertCurrency.js
+++ src/utils/currencies/convertCurrency.js
@@ -4,6 +4,6 @@ export default function convertCurrency(rates, from, to, value) {
 
   // Convert price
   const toNewCurrency = value / rates[from]
-  return roundTo(toNewCurrency * rates[to], 2)
+  return (roundTo(toNewCurrency * rates[to], 2)).toFixed(2)
 
 }
