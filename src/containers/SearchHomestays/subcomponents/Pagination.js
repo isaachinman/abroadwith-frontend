9e87679c950b31bbed21dc374215a68812ebcbe5
isaachinman <isaacshinman@gmail.com>
2017-02-15 10:42:10 +0100

    Small improvements/bug fixes to SearchHomestays

--- src/containers/SearchHomestays/subcomponents/Pagination.js
+++ src/containers/SearchHomestays/subcomponents/Pagination.js
@@ -12,12 +12,30 @@ import { performRoomSearch } from 'redux/modules/ui/search/homestaySearch'
 )
 export default class SearchPagination extends Component {
 
+  componentWillReceiveProps = (nextProps) => {
+
+    // If a user is on a page greater than one and performs a new search that returns less results,
+    // put them back on page 1 to be safe
+    if (((this.props.homestaySearch.params.pageOffset / this.props.homestaySearch.params.pageSize) + 1) > 1 && this.props.homestaySearch.data.resultDetails.numberOfResults !== nextProps.homestaySearch.data.resultDetails.numberOfResults) {
+      this.props.dispatch(performRoomSearch(Object.assign({}, this.props.homestaySearch.params, {
+        pageOffset: 0,
+      })))
+    }
+
+  }
+
   handleChange = page => {
+
+    // Dispatch new search
     const { dispatch, homestaySearch } = this.props
     const newParams = Object.assign({}, homestaySearch.params, {
       pageOffset: (homestaySearch.params.pageSize * page) - homestaySearch.params.pageSize,
     })
     dispatch(performRoomSearch(newParams))
+
+    // Scroll result list to top
+    document.getElementById('homestay-search-result-list').scrollTop = 0
+
   }
 
   render() {
