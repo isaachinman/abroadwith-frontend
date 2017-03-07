5f8ed688470bcf9f2da2f2649c62d522ee9844ca
isaachinman <isaacshinman@gmail.com>
2017-03-07 10:17:37 +0100

    AW2-140

--- src/containers/Invite/Invite.styles.js
+++ src/containers/Invite/Invite.styles.js
@@ -1,29 +1,6 @@
-import { accentGrey1, accentGrey3 } from 'styles/colors'
-
 export default {
-  loginPage: {
+  topRow: {
+    margin: '60px 0',
     textAlign: 'center',
-    paddingTop: 50,
-  },
-  loginPanel: {
-    padding: '20px 20px 10px 20px',
-  },
-  paddedRow: {
-    paddingBottom: 15,
-  },
-  dividerContainer: {
-    height: 50,
-  },
-  divider: {
-    height: 20,
-    borderBottom: `1px solid ${accentGrey1}`,
-    textAlign: 'center',
-  },
-  dividerText: {
-    fontSize: 14,
-    color: accentGrey3,
-    backgroundColor: 'white',
-    padding: '0 10px',
-    lineHeight: '35px',
   },
 }
