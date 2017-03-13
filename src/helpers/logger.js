570dd6938efeef4f3d3ba7cd0499a701e680a21a
isaachinman <isaacshinman@gmail.com>
2017-03-13 14:05:41 +0100

    Write processID into log path

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -4,15 +4,15 @@ import moment from 'moment'
 const winston = require('winston')
 const S3StreamLogger = require('s3-streamlogger').S3StreamLogger
 
-const date = moment().format('YYYY-MM-DD')
+// Both date a processID are needed to name files (clusters would otherwise overwrite each other)
 
 // S3 bucket config
 const s3_stream = new S3StreamLogger({
   bucket: 'abroadwith-logs',
-  folder: `frontend/${date}`,
+  folder: `frontend/${moment().format('YYYY-MM-DD')}`,
   access_key_id: 'AKIAIH5LVMZRVT7ZENCA',
   secret_access_key: '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',
-  name_format: `abroadwith_frontend_logs_${date}_%H:%M.log`,
+  name_format: `abroadwith_frontend_logs_${moment().format('YYYY-MM-DD')}_%H:%M_(${process.pid}).log`,
   rotate_every: 3600000, // Rotates every hour
   config: {
     region: 'eu-central-1',
@@ -20,9 +20,13 @@ const s3_stream = new S3StreamLogger({
 })
 
 // Bind stream error to different function to prevent recursion
-s3_stream.on('error', (err) => console.log(err))
+// s3_stream.on('error', (err) => console.log(err))
 
 const transports = []
+
+transports.push(
+  new (winston.transports.Console)()
+)
 
 // Only log to S3 in production
 if (process.env.NODE_ENV === 'production') {
@@ -30,10 +34,6 @@ if (process.env.NODE_ENV === 'production') {
     new (winston.transports.File)({
       stream: s3_stream,
     }),
-  )
-} else {
-  transports.push(
-    new (winston.transports.Console)()
   )
 }
 
