439bc4b260223ce3398cd2cf9e203df8d11e822a
isaachinman <isaacshinman@gmail.com>
2017-03-15 17:12:48 +0100

    SearchCourses zoom bug fixed

--- src/helpers/api/getClosestCity.js
+++ src/helpers/api/getClosestCity.js
@@ -6,8 +6,6 @@ export default (app) => {
   // This is the public endpoint to return the closest (within 50km) city to a given lat/lng point
   app.post('/public/closest-city', (req, res) => {
 
-    console.log('REQUEST BODY: ', req.body)
-
     const request = superagent.get(`${config.solr.host}:${config.solr.port}/solr/abroadwith_cities/select?q=*&wt=json&fq={!geofilt%20pt=${req.body.lat},${req.body.lng}%20sfield=location%20d=50}`)
     request.end((error, response = {}) => {
 
@@ -18,7 +16,6 @@ export default (app) => {
       }
 
           // GET was successful
-      console.log('RESPONSE: ', JSON.parse(response.text).response)
       const parsedResponse = JSON.parse(response.text).response
 
       if (parsedResponse.numFound === 1) {
