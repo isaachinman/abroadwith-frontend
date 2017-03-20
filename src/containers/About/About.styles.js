5f082edd274717cf2289ce3ef25319be03f92530
isaachinman <isaacshinman@gmail.com>
2017-03-20 12:00:13 +0100

    AW2-176

--- src/containers/About/About.styles.js
+++ src/containers/About/About.styles.js
@@ -11,4 +11,19 @@ export default {
     margin: '60px 0 30px 0',
     textAlign: 'center',
   },
+  parterRow: {
+    position: 'relative',
+    boxShadow: '3px 3px 7px 0 rgba(0,0,0,0.15)',
+    zIndex: 2,
+    background: '#eee',
+    marginBottom: 0,
+  },
+  partnerImg: {
+    opacity: 0.9,
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    maxWidth: 200,
+    maxHeight: 180,
+    margin: '50px 30px',
+  },
 }
