afb2b58141bd0c33a9455791e041fbd33a1dcc5a
Isaac Hinman <isaac@isaachinman.com>
2017-04-04 13:17:22 +0200

    Days of week are zero indexed

--- src/containers/School/subcomponents/Course.js
+++ src/containers/School/subcomponents/Course.js
@@ -14,13 +14,13 @@ import styles from '../School.styles'
 
 // Days of week map
 const daysOfTheWeek = {
-  1: 'mon',
-  2: 'tue',
-  3: 'wed',
-  4: 'thu',
-  5: 'fri',
-  6: 'sat',
-  7: 'sun',
+  0: 'mon',
+  1: 'tue',
+  2: 'wed',
+  3: 'thu',
+  4: 'fri',
+  5: 'sat',
+  6: 'sun',
 }
 
 @connect(
